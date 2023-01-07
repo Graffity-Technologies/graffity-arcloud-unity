56eb9efc6f8c80d037fa41e6abe1b8f5662088c8
bankbiz <thaworn@graffity.tech>
2023-01-07 21:36:51 +0700

    Release version 0.2.5

--- Runtime/Grpc/GrpcClientImage.cs
+++ Runtime/Grpc/GrpcClientImage.cs
@@ -4,17 +4,17 @@ using Vpsimage;
 
 namespace UnityEngine.Graffity.ARCloud
 {
-    public class GrpcClientImage: GrpcClientBase
+    public class GrpcClientImage : GrpcClientBase
     {
         private Action<ImageResponse> callBackActionReply;
         public GrpcClientImage(string host, string apiKey) : base(host, apiKey)
         {
         }
-        
+
         public override async Task<object> SendGrpcAsync<TRequest>(TRequest info)
         {
             var client = new Vpsimage.Image.ImageClient(grpcChannel);
-            var reply = await client.SendImageAsync(info as ImageRequest, headers:grpcHeader, deadline: DateTime.UtcNow.AddSeconds(20));
+            var reply = await client.SendImageAsync(info as ImageRequest, headers: grpcHeader, deadline: DateTime.UtcNow.AddSeconds(60));
             return reply;
         }
     }
